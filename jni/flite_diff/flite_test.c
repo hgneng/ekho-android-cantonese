58b4b616d32d78088733b55de70a8d283d097679
黄冠能 <hgneng@gmail.com>
2023-01-30 09:53:56 +0800

    arm64-v8a build fixed

---
+++ jni/flite_diff/flite_test.c
@@ -0,0 +1,22 @@
+// gcc -Wall -g -o flite_test flite_test.c -Iinclude build/x86_64-darwin21.6.0/lib/*.a
+
+#include "flite.h"
+cst_voice *register_cmu_us_kal(const char *voxdir);
+
+int main(int argc, char **argv)
+{
+    cst_voice *v;
+
+    if (argc != 2)
+    {
+        fprintf(stderr,"usage: flite_test FILE\n");
+        exit(-1);
+    }
+
+    flite_init();
+
+    v = register_cmu_us_kal(NULL);
+
+    flite_file_to_speech(argv[1],v,"hello.wav");
+
+}
